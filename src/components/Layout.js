53e5e8cb129bb9720d645ad1815e7d8d73391da2
Turiano Romero <romero.guzman18@gmail.com>
2019-08-16 15:26:55 -0500

    agg iconts

---
+++ src/components/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MyDrawer from './Drawer';
+
+class Layout extends React.Component {
+  render(){
+    return(
+      <div>
+       <MyDrawer/>
+      </div>
+    )
+  }
+}
+
+export default Layout
---
+++ src/components/nteractions.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Interactions = () => {
+  return(
+    <div>
+      <h1>Hola desde interactions</h1>
+    </div>
+  )
+}
+
+export default Interactions
