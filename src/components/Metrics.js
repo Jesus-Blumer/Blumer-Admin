3e06748d357aaeb65e1c5bdd94437d307749c263
Turiano Romero <romero.guzman18@gmail.com>
2019-08-17 12:07:22 -0500

    metricts

--- src/components/Metrics.js
+++ src/components/Metrics.js
@@ -30,53 +30,53 @@ class Metrics extends React.Component {
   render() {
     return (
       <div>
-       <div>
-       <Chart
-          width={"95%"}
-          height={"195%"}
-          chartType="PieChart"
-          loader={<div>Loading Chart</div>}
-          data={[
-            ["Year", "Sales", "Expenses", "Profit"],
-            ["2014", 1000, 400, 200],
-            ["2015", 1170, 460, 250],
-            ["2016", 660, 1120, 300],
-            ["2017", 1030, 540, 350]
-          ]}
-          options={{
-            // Material design options
-            chart: {
-              title: "Company Performance",
-              subtitle: "Sales, Expenses, and Profit: 2014-2017"
-            }
-          }}
-          // For tests
-          rootProps={{ "data-testid": "2" }}
-        />
-       </div>
         <div>
-        <Chart
-          width={"95%"}
-          height={"195%"}
-          chartType="BarChart"
-          loader={<div>Loading Chart</div>}
-          data={[
-            ["Year", "Sales", "Expenses", "Profit"],
-            ["2014", 1000, 400, 200],
-            ["2015", 1170, 460, 250],
-            ["2016", 660, 1120, 300],
-            ["2017", 1030, 540, 350]
-          ]}
-          options={{
-            // Material design options
-            chart: {
-              title: "Company Performance",
-              subtitle: "Sales, Expenses, and Profit: 2014-2017"
-            }
-          }}
-          // For tests
-          rootProps={{ "data-testid": "2" }}
-        />
+          <Chart
+            width={"95%"}
+            height={"195%"}
+            chartType="PieChart"
+            loader={<div>Loading Chart</div>}
+            data={[
+              ["Year", "Sales", "Expenses", "Profit"],
+              ["2014", 1000, 400, 200],
+              ["2015", 1170, 460, 250],
+              ["2016", 660, 1120, 300],
+              ["2017", 1030, 540, 350]
+            ]}
+            options={{
+              // Material design options
+              chart: {
+                title: "Company Performance",
+                subtitle: "Sales, Expenses, and Profit: 2014-2017"
+              }
+            }}
+            // For tests
+            rootProps={{ "data-testid": "2" }}
+          />
+        </div>
+        <div>
+          <Chart
+            width={"95%"}
+            height={"195%"}
+            chartType="BarChart"
+            loader={<div>Loading Chart</div>}
+            data={[
+              ["Year", "Sales", "Expenses", "Profit"],
+              ["2014", 1000, 400, 200],
+              ["2015", 1170, 460, 250],
+              ["2016", 660, 1120, 300],
+              ["2017", 1030, 540, 350]
+            ]}
+            options={{
+              // Material design options
+              chart: {
+                title: "Company Performance",
+                subtitle: "Sales, Expenses, and Profit: 2014-2017"
+              }
+            }}
+            // For tests
+            rootProps={{ "data-testid": "2" }}
+          />
         </div>
       </div>
     );
